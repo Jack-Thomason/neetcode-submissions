ab99c81db0d2ad5172fe78583758c5295ffe8c29
Jack Thomason <119045746+Jack-Thomason@users.noreply.github.com>
2026-07-13 18:06:35 +0100

    Add: count-subsequences - submission-3

---
+++ Data Structures & Algorithms/count-subsequences/submission-3.py
@@ -0,0 +1,26 @@
+from functools import cache
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        # top-down improved
+
+        m, n = len(s), len(t)
+
+        @cache
+        def dfs(i, j):
+            if j == n:
+                return 1
+            if i == m:
+                return 0
+
+            if m - i < n - j:
+                return 0
+            
+            # skip
+            ways = dfs(i + 1, j)
+
+            if s[i] == t[j]:
+                ways += dfs(i + 1, j + 1)
+            
+            return ways
+        
+        return dfs(0, 0)
